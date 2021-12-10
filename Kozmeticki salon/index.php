b53b6cabb663f6955e0ab418612d05461b47cc9c
Kristina36916 <kristina.ivancuk@gmail.com>
2021-12-10 23:49:53 +0100

    kreiranje stranice o nama

--- Kozmeticki salon/index.php
+++ Kozmeticki salon/index.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Small Business - Start Bootstrap Template</title>
+        <title>Kozmeticki salon</title>
         <!-- Favicon-->
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
         <!-- Core theme CSS (includes Bootstrap)-->
@@ -19,8 +19,8 @@
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#!">O nama</a></li>
+                        <li class="nav-item"><a class="nav-link active"  href="./index.php">Početna</a></li>
+                        <li class="nav-item"><a class="nav-link" href="./onama.html">O nama</a></li>
                         <li class="nav-item"><a class="nav-link" href="#!">Cenovnik</a></li>
                         <li class="nav-item"><a class="nav-link" href="#!">Zakazivanje</a></li>
                         <li class="nav-item"><a class="nav-link" href="#!">Zakazani termini</a></li>

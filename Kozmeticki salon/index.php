420cb8ccdeeba3d0c7a7a383ce8ba619787dff5b
Kristina36916 <kristina.ivancuk@gmail.com>
2021-12-10 21:25:46 +0100

    inicjalizovan kostur

---
+++ Kozmeticki salon/index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Small Business - Start Bootstrap Template</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body>
+        <!-- Responsive navbar-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container px-5">
+                <a class="navbar-brand" href="#!">Shine bright like a diamond</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">O nama</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">Cenovnik</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">Zakazivanje</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">Zakazani termini</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">Kontakt</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Page Content-->
+        <div class="container px-4 px-lg-5">
+            <!-- Heading Row-->
+            <div class="row gx-4 gx-lg-5 align-items-center my-5">
+                <div class="col-lg-7"><img class="img-fluid rounded mb-4 mb-lg-0" src="assets/slika1.jpg" alt="..." /></div>
+                <div class="col-lg-5">
+                    <h1 class="font-weight-light">Kozmetički salon</h1>
+                    <p>Novootvoreni kozmetički salon je Vaša nova oaza gde se možete opustiti,prolepšati i učiniti mnogo toga za svoju dušu i telo.Kozmetički salon čini više celina, gde na jednom mestu možete uraditi sve potrebne tretmane i tako uštedeti vreme i novac.
+Nov i moderno opremljen prostor, stručno osoblje i uvek prijatna atmosfera, obezbeđen parking, dvorište i kutak za kijente za kaficu su sitnice koje čine razliku.</p>
+                    <a class="btn btn-primary" href="#!">Cenovnik</a>
+                </div>
+            </div>
+            <!-- Call to Action-->
+            <div class="card text-white bg-secondary my-5 py-4 text-center">
+                <div class="card-body"><p class="text-white m-0"></p><h4>Zakažite Vaš termin na vreme!</h4>
+            </div>
+            </div>
+            <!-- Content Row-->
+            <div class="row gx-4 gx-lg-5">
+                <div class="col-md-4 mb-5">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h2 class="card-title">Tretmani lica</h2>
+                            <p class="card-text">Koža, kao naš najveći organ, predstavlja zaštitnu barijeru koja nas štiti od negativnih spoljašnjih uticaja. Usled izloženosti zagađenom vazduhu, ultraljubičastom zračenju, duvanskom dimu i svakodnevnim nanošenjem šminke struktura kože zadebljava, pune se pore, koža gubi svežinu i poprima umoran izgled. </p>
+                        </div>
+                        <div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">Cenovnik</a></div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-5">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h2 class="card-title">Masaže</h2>
+                            <p class="card-text">Masaža je jedan od najstarijih i najjednostavnijih načina lečenja. Ime joj potiče od arapske reči “Massa”, što znači dodir.Dodir je suština i osnova masaže. Dodir znači vezu, odnos sa onim sto je izvan nas.Za čoveka je dodir od bitne važnosti jer pruža sigurnost, toplinu, zadovoljstvo, ugodnost i vraća životnu snagu.</p>
+                        </div>
+                        <div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">Cenovnik</a></div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-5">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h2 class="card-title">Laserska epilacija</h2>
+                            <p class="card-text">Jedina u svetu priznata neinvazivna metoda trajnog uklanjanja malja (trajna epilacija) je epilacija laserom. Pored nje postoji još samo elektroliza, ali ona spada u invazivne metode. Laser za epilaciju se koristi već petnaestak godina u Americi, pa onda u svetu.</p>
+                        </div>
+                        <div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">Cenovnik</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Footer-->
+        <footer class="py-5 bg-dark">
+            <div class="container px-4 px-lg-5"><p class="m-0 text-center text-white">Copyright &copy; Kristina 2021</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
